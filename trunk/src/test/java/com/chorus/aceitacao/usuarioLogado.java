2c85b6b67191841f3dfaef92a657aa2dc43dcd4e
paulojribp@gmail.com <paulojribp@gmail.com@0e391655-1244-7e70-1e65-91e2530d3295>
2013-05-18 19:38:55 +0000

    Removendo comentários. Caso de uso funcionando..

--- trunk/src/test/java/com/chorus/aceitacao/usuarioLogado.java
+++ trunk/src/test/java/com/chorus/aceitacao/usuarioLogado.java
@@ -10,24 +10,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.thoughtworks.selenium.SeleneseTestCase;
 
 public class usuarioLogado extends SeleneseTestCase {
-//	@Before
-//	public void setUp() throws Exception {
-//		WebDriver driver = new FirefoxDriver();
-//		String baseUrl = "http://54.243.139.54/";
-//		selenium = new WebDriverBackedSelenium(driver, baseUrl);
-//	}
-//
-//	@Test
-//	public void testUsuarioLogado() throws Exception {
-//		selenium.open("/chorus/index/index");
-//		selenium.type("id=loginUsername", "flavioso");
-//		selenium.type("id=loginSenha", "123456");
-//		selenium.click("id=btn-usuario-login");
-//		selenium.waitForPageToLoad("30000");
-//	}
-//
-//	@After
-//	public void tearDown() throws Exception {
-////		selenium.stop();
-////	}
+	@Before
+	public void setUp() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://54.243.139.54/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+
+	@Test
+	public void testUsuarioLogado() throws Exception {
+		selenium.open("/chorus/index/index");
+		selenium.type("id=loginUsername", "flavioso");
+		selenium.type("id=loginSenha", "123456");
+		selenium.click("id=btn-usuario-login");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
 }
