c8fc1d2b395c1b22a7492f81b3b792cb6c8778f9
flaviosof@gmail.com <flaviosof@gmail.com@0e391655-1244-7e70-1e65-91e2530d3295>
2013-05-18 17:33:52 +0000

    comentado teste

--- trunk/src/test/java/com/chorus/aceitacao/LogarUsuario.java
+++ trunk/src/test/java/com/chorus/aceitacao/LogarUsuario.java
@@ -8,31 +8,31 @@ import com.thoughtworks.selenium.SeleneseTestCase;
 
 public class LogarUsuario extends SeleneseTestCase {
 
-	@Before
-		public void setUp() throws Exception {
-			selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://54.243.139.54/");
-			selenium.start();
-		}
-
-		@Test
-		public void testLogarUsuario() throws Exception {
-			selenium.open("/chorus/index/index");
-			selenium.type("id=nome", "flavioso");
-			selenium.type("id=email", "flaviosof@gmail.c");
-			selenium.type("id=username", "flavioso");
-			selenium.type("id=senha", "123456");
-			selenium.type("id=confirmasenha", "123456");
-			selenium.click("id=btn-cadastrar");
-			selenium.type("id=nome", "flaviosof");
-			selenium.click("id=btn-cadastrar");
-			selenium.type("id=username", "flaviosoceara");
-			selenium.click("id=btn-cadastrar");
-		}
-
-		@After
-		public void tearDown() throws Exception {
-			selenium.stop();
-		}
+//	@Before
+//		public void setUp() throws Exception {
+//			selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://54.243.139.54/");
+//			selenium.start();
+//		}
+//
+//		@Test
+//		public void testLogarUsuario() throws Exception {
+//			selenium.open("/chorus/index/index");
+//			selenium.type("id=nome", "flavioso");
+//			selenium.type("id=email", "flaviosof@gmail.c");
+//			selenium.type("id=username", "flavioso");
+//			selenium.type("id=senha", "123456");
+//			selenium.type("id=confirmasenha", "123456");
+//			selenium.click("id=btn-cadastrar");
+//			selenium.type("id=nome", "flaviosof");
+//			selenium.click("id=btn-cadastrar");
+//			selenium.type("id=username", "flaviosoceara");
+//			selenium.click("id=btn-cadastrar");
+//		}
+//
+//		@After
+//		public void tearDown() throws Exception {
+//			selenium.stop();
+//		}
 	}
 
 	
