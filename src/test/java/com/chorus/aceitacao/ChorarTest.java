0e761a0ef3117ecbc31eab3cc645cddf5fa39880
ketemartinsrufino <ketemartinsrufino@0e391655-1244-7e70-1e65-91e2530d3295>
2013-05-22 21:19:23 +0000

    Melhorias do código dos testes selenium.

--- src/test/java/com/chorus/aceitacao/ChorarTest.java
+++ src/test/java/com/chorus/aceitacao/ChorarTest.java
@@ -1,32 +1,13 @@
 package com.chorus.aceitacao;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverBackedSelenium;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import com.thoughtworks.selenium.Selenium;
-
-public class ChorarTest {
+public class ChorarTest extends AbstractSeleniumChorusTest {
 	
-	private static WebDriver driver;
-	private static Selenium selenium;
-	
-	@Before
-	public void setUp(){
-		driver = new FirefoxDriver();
-		selenium = new WebDriverBackedSelenium(driver,"http://54.243.139.54/");
-		driver.get("http://54.243.139.54/");	
-	}
 
 	@Test
 	public void testChorarTeste() throws Exception {
-		selenium.open("/chorus/index/index");
-		selenium.type("id=loginUsername", "flavioso");
-		selenium.type("id=loginSenha", "123456");
-		selenium.click("id=btn-usuario-login");
+		login("kete", "123456");
 		selenium.waitForPageToLoad("30000");
 		selenium.type("id=escrever-chorinho", "teste tricampeao...");
 		selenium.click("id=btn-chorar");
@@ -34,8 +15,4 @@ public class ChorarTest {
 		selenium.waitForPageToLoad("30000");
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		selenium.stop();
-	}
 }
--- src/test/java/com/chorus/aceitacao/ChorusUrlEncurtadaTest.java
+++ src/test/java/com/chorus/aceitacao/ChorusUrlEncurtadaTest.java
@@ -1,42 +1,16 @@
 package com.chorus.aceitacao;
 
-import com.thoughtworks.selenium.Selenium;
-import com.thoughtworks.selenium.SeleneseTestCase;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverBackedSelenium;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import java.util.regex.Pattern;
 
-public class ChorusUrlEncurtadaTest {
+public class ChorusUrlEncurtadaTest extends AbstractSeleniumChorusTest{
 	
-	private static WebDriver driver;
-	private static Selenium selenium;
-	
-	@Before
-	public void setUp(){
-		driver = new FirefoxDriver();
-		selenium = new WebDriverBackedSelenium(driver,"http://54.243.139.54/");
-		driver.get("http://54.243.139.54/");	
-	}
-
 	@Test
 	public void testChorusUrlEncurtada() throws Exception {
-		selenium.open("/chorus/index/index");
-		selenium.type("id=loginUsername", "flavioso");
-		selenium.type("id=loginSenha", "123456");
-		selenium.click("id=btn-usuario-login");
+		login("kete", "123456");
 		selenium.waitForPageToLoad("30000");
 		selenium.type("id=escrever-chorinho", "http://wwww.fa7.edu.br/teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
 		selenium.click("id=btn-chorar");
 		selenium.click("link=Chorinhos");
 		selenium.waitForPageToLoad("30000");
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		selenium.stop();
 	}
 }
