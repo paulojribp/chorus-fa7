1c9c40c540b98814a3603bdc6f89b998b26e4fed
flaviosof@gmail.com <flaviosof@gmail.com@0e391655-1244-7e70-1e65-91e2530d3295>
2013-05-22 13:54:44 +0000

    alterado prara ser executado pelo junit

---
+++ trunk/src/test/java/com/example/tests/chorarTeste.java
@@ -0,0 +1,33 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class chorarTeste extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://54.243.139.54/");
+		selenium.start();
+	}
+
+	@Test
+	public void testChorarTeste() throws Exception {
+		selenium.open("/chorus/index/index");
+		selenium.type("id=loginUsername", "flavioso");
+		selenium.type("id=loginSenha", "123456");
+		selenium.click("id=btn-usuario-login");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("id=escrever-chorinho", "teste tricampeao...");
+		selenium.click("id=btn-chorar");
+		selenium.click("link=Chorinhos");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
